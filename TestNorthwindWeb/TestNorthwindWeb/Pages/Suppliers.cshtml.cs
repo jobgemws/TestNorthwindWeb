4719e716c2216129b4e5491e803a881a986b8ecb
Evgeniy Gribkov <37021038+jobgemws@users.noreply.github.com>
2019-07-23 22:46:51 +0300

    2h
    lab work (chapter 14 of the book C # 7.0 .NET Core 2.0)

---
+++ TestNorthwindWeb/TestNorthwindWeb/Pages/Suppliers.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TestNorthwindContextLib;
+using Microsoft.AspNetCore.Mvc;
+using TestNorthwindEntitiesLib;
+
+namespace TestNorthwindWeb.Pages
+{
+    public class SuppliersModel : PageModel
+    {
+        private Northwind db;
+
+        public SuppliersModel(Northwind injectedContext)
+        {
+            db = injectedContext;
+        }
+        public IEnumerable<string> Suppliers { get; set; }
+        public void OnGet()
+        {
+            ViewData["Title"] = "Northwind Web Site - Suppliers";
+            //Suppliers = new[] { "Alpha Co", "Beta Limited", "Gamma Corp" };
+            Suppliers = db.Suppliers.Select(s => s.CompanyName).ToArray();
+        }
+
+        [BindProperty]
+        public Supplier Supplier { get; set; }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                db.Suppliers.Add(Supplier);
+                db.SaveChanges();
+                return RedirectToPage("/suppliers");
+            }
+            return Page();
+        }
+    }
+}
